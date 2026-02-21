fc8e4f30b2656bf4ca1f85f3e891cc00ace4e5c9
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-21 21:47:02 +0530

    f

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"]
     })
 )
 
-app.use(express.json())
+app.use(express.json()) 
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "dist")));
 
 connectDB();
 
